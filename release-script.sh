8396e337c9d19012f87ae8e797739ee69524d180
Eric <eric@bostrot.com>
2021-09-18 17:24:26 +0200

    Update CI

--- release-script.sh
+++ release-script.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-wget https://github.com/cli/cli/releases/download/v2.0.0/gh_2.0.0_linux_amd64.deb
-dpkg -i gh_2.0.0_linux_amd64.deb
-echo $GITHUB_TOKEN > token
-gh auth login --with-token < token
+flutter build windows
 version=$(cat pubspec.yaml | grep -o -P '(?<=version: ).*(?= #)')
-echo $version
-apt-get update && apt-get install -y zip wget
-pwd
-echo 'cp ./windows-dlls/* ./build/windows/runner/Release'
-echo 'cd ./build/windows/runner/Release'
-echo 'zip -r wsl2-distro-manager-v' . $version . '.zip .'
-echo 'gh release create v' . $version . './build/windows --notes "This is an automated release."'
-echo 'flutter build windows'
+cp ./windows-dlls/* ./build/windows/runner/Release
+cd ./build/windows/runner/Release
+zip -r wsl2-distro-manager-v$version.zip .
+gh release create v$version ./build/windows/runner/Release/wsl2-distro-manager-v$version.zip --notes "This is an automated release."
