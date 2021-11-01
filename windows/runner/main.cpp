8f6ae85d7a37a77884ad31747949f08d19cb02fb
Eric <eric@bostrot.com>
2021-11-01 18:35:57 +0100

    Fixed cmd opening in release

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -10,8 +10,28 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
-    CreateAndAttachConsole();
+      CreateAndAttachConsole();
+  } else {
+    // see https://github.com/flutter/flutter/issues/47891
+    STARTUPINFO si = { 0 };
+    si.cb = sizeof(si);
+    si.dwFlags = STARTF_USESHOWWINDOW;
+    si.wShowWindow = SW_HIDE;
+
+    PROCESS_INFORMATION pi = { 0 };
+    WCHAR lpszCmd[MAX_PATH] = L"cmd.exe";
+    if (::CreateProcess(NULL, lpszCmd, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | CREATE_NO_WINDOW, NULL, NULL, &si, &pi)) {
+      do {
+        if (::AttachConsole(pi.dwProcessId)) {
+          ::TerminateProcess(pi.hProcess, 0);
+          break;
+        }
+      } while (ERROR_INVALID_HANDLE == GetLastError());
+      ::CloseHandle(pi.hProcess);
+      ::CloseHandle(pi.hThread);
+    }
   }
+
 
   // Initialize COM, so that it is available for use in the library and/or
   // plugins.
