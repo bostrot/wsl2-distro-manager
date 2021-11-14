fce79bf66e8f07fd5eb1869a2977827df0a0b17a
Eric <eric@bostrot.com>
2021-11-14 02:29:38 +0100

    Added tag check

--- release-script.sh
+++ release-script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# get version from pubspec
 version=$(cat pubspec.yaml | grep -o -P '(?<=version: ).*(?= #)')
+
+# copy files
 cp ./windows-dlls/* ./build/windows/runner/Release
 cd ./build/windows/runner/Release
 zip -r wsl2-distro-manager-v$version.zip .
-gh release create v$version wsl2-distro-manager-v$version.zip --notes "This is an automated release."
+
+# check tag already exists
+if [ $(echo $(gh release view --json tagName) | sed -e 's/{"tagName":"\(.*\)"}/\1/') != v$version ]; 
+then 
+gh release create v$version wsl2-distro-manager-v$version.zip --notes "This is an automated release." 
+fi
