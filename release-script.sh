4ae2cb35bcacac1148fac9e668362e3b2a6bbba2
Eric <me@bostrot.com>
2022-01-07 22:29:50 +0100

    Update release-script.sh

--- release-script.sh
+++ release-script.sh
@@ -8,7 +8,7 @@ cd ./build/windows/runner/Release
 zip -r wsl2-distro-manager-v$version.zip .
 
 # check tag already exists
-if [ $(echo $(gh release view --json tagName) | sed -e 's/{"tagName":"\(.*\)"}/\1/') != v$version ]; 
+if [ "$(echo $(gh release view --json tagName) | sed -e 's/{"tagName":"\(.*\)"}/\1/')" != "v$version" ]; 
 then 
 gh release create v$version wsl2-distro-manager-v$version.zip --notes "This is an automated release." 
 fi
