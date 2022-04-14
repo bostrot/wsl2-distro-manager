a02a24d2474d542d80fa6f5470933e6023a1e398
Eric <me@bostrot.com>
2022-04-14 16:18:07 +0200

    Stateful widget, turnkey detection, fake systemctl

--- lib/dialogs/create_dialog.dart
+++ lib/dialogs/create_dialog.dart
@@ -21,120 +21,15 @@ createDialog(context, Function(String, {bool loading}) statusMsg) {
     context: context,
     builder: (context) {
       return ContentDialog(
+        constraints: const BoxConstraints(maxHeight: 450.0, maxWidth: 400.0),
         title: const Text('Create a new distro'),
-        content: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 10.0,
-            ),
-            const Text(
-              'Name:',
-            ),
-            Container(
-              height: 5.0,
-            ),
-            Tooltip(
-              message: 'The name of your new WSL instance',
-              child: TextBox(
-                controller: nameController,
-                placeholder: 'Name',
-                suffix: IconButton(
-                  icon: const Icon(FluentIcons.chrome_close, size: 15.0),
-                  onPressed: () {
-                    nameController.clear();
-                  },
-                ),
-              ),
-            ),
-            Container(
-              height: 10.0,
-            ),
-            const Text(
-              'Path to rootfs or distro name:',
-            ),
-            Container(
-              height: 5.0,
-            ),
-            Tooltip(
-              message:
-                  'Either use one of the pre-defined Distros or a file path to a rootfs',
-              child: FutureBuilder<List<String>>(
-                  future: api.getDownloadable(),
-                  builder: (context, snapshot) {
-                    List<String> list = [];
-                    if (snapshot.hasData) {
-                      list = snapshot.data ?? [];
-                    } else if (snapshot.hasError) {}
-                    return AutoSuggestBox(
-                      placeholder: 'Distro name or path to rootfs',
-                      controller: autoSuggestBox,
-                      items: list,
-                      trailingIcon: IconButton(
-                        icon: const Icon(FluentIcons.open_folder_horizontal,
-                            size: 15.0),
-                        onPressed: () async {
-                          FilePickerResult? result =
-                              await FilePicker.platform.pickFiles(
-                            type: FileType.custom,
-                            allowedExtensions: ['*'],
-                          );
-
-                          if (result != null) {
-                            autoSuggestBox.text = result.files.single.path!;
-                          } else {
-                            // User canceled the picker
-                          }
-                        },
-                      ),
-                    );
-                  }),
-            ),
-            Container(
-              height: 10.0,
-            ),
-            const Text(
-              'Save location:',
-            ),
-            Container(
-              height: 5.0,
-            ),
-            Tooltip(
-              message: '(Optional) Path where to save the new instance',
-              child: TextBox(
-                controller: locationController,
-                placeholder: 'Save location (optional)',
-                suffix: IconButton(
-                  icon: const Icon(FluentIcons.open_folder_horizontal,
-                      size: 15.0),
-                  onPressed: () async {
-                    String? path = await FilePicker.platform.getDirectoryPath();
-                    if (path != null) {
-                      locationController.text = path;
-                    } else {
-                      // User canceled the picker
-                    }
-                  },
-                ),
-              ),
-            ),
-            Container(
-              height: 10.0,
-            ),
-            const Text(
-              'Create default user (only on Debian/Ubuntu):',
-            ),
-            Container(
-              height: 5.0,
-            ),
-            Tooltip(
-              message: '(Optional) Username',
-              child: TextBox(
-                controller: userController,
-                placeholder: '(Optional) User',
-              ),
-            ),
-          ],
+        content: SingleChildScrollView(
+          child: CreateWidget(
+              nameController: nameController,
+              api: api,
+              autoSuggestBox: autoSuggestBox,
+              locationController: locationController,
+              userController: userController),
         ),
         actions: [
           Button(
@@ -189,8 +84,23 @@ createDialog(context, Function(String, {bool loading}) statusMsg) {
                           'WARNING: Created instance but failed to create user');
                     }
                   } else {
-                    Navigator.pop(context);
-                    statusMsg('DONE: creating instance');
+                    if (Navigator.canPop(context)) {
+                      Navigator.pop(context);
+                    }
+                    statusMsg('Installing fake systemd ...');
+                    // Install fake systemctl
+                    if (autoSuggestBox.text.contains('Turnkey')) {
+                      WSLApi().execCmds(
+                          name,
+                          [
+                            'wget https://raw.githubusercontent.com/bostrot/'
+                                'fake-systemd/master/systemctl -O /usr/bin/systemctl',
+                            'chmod +x /usr/bin/systemctl',
+                            '/usr/bin/systemctl',
+                          ],
+                          onMsg: (output) => print(output),
+                          onDone: () => statusMsg('DONE: creating instance'));
+                    }
                   }
                   // Save distro label
                   prefs.setString('DistroName_' + name, label);
@@ -208,4 +118,178 @@ createDialog(context, Function(String, {bool loading}) statusMsg) {
       );
     },
   );
+}
+
+class CreateWidget extends StatefulWidget {
+  const CreateWidget({
+    Key? key,
+    required this.nameController,
+    required this.api,
+    required this.autoSuggestBox,
+    required this.locationController,
+    required this.userController,
+  }) : super(key: key);
+
+  final TextEditingController nameController;
+  final WSLApi api;
+  final TextEditingController autoSuggestBox;
+  final TextEditingController locationController;
+  final TextEditingController userController;
+
+  @override
+  State<CreateWidget> createState() => _CreateWidgetState();
+}
+
+class _CreateWidgetState extends State<CreateWidget> {
+  bool turnkey = false;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 10.0,
+        ),
+        const Text(
+          'Name:',
+        ),
+        Container(
+          height: 5.0,
+        ),
+        Tooltip(
+          message: 'The name of your new WSL instance',
+          child: TextBox(
+            controller: widget.nameController,
+            placeholder: 'Name',
+            suffix: IconButton(
+              icon: const Icon(FluentIcons.chrome_close, size: 15.0),
+              onPressed: () {
+                widget.nameController.clear();
+              },
+            ),
+          ),
+        ),
+        Container(
+          height: 10.0,
+        ),
+        const Text(
+          'Path to rootfs or distro name:',
+        ),
+        Container(
+          height: 5.0,
+        ),
+        Tooltip(
+          message:
+              'Either use one of the pre-defined Distros or a file path to a '
+              'rootfs',
+          child: FutureBuilder<List<String>>(
+              future: widget.api.getDownloadable(),
+              builder: (context, snapshot) {
+                List<String> list = [];
+                if (snapshot.hasData) {
+                  list = snapshot.data ?? [];
+                } else if (snapshot.hasError) {}
+                return AutoSuggestBox(
+                  placeholder: 'Distro name or path to rootfs',
+                  controller: widget.autoSuggestBox,
+                  items: list,
+                  onChanged: (String value, TextChangedReason reason) {
+                    if (value.contains('Turnkey')) {
+                      if (!turnkey) {
+                        setState(() {
+                          turnkey = true;
+                        });
+                      }
+                    } else {
+                      if (turnkey) {
+                        setState(() {
+                          turnkey = false;
+                        });
+                      }
+                    }
+                  },
+                  trailingIcon: IconButton(
+                    icon: const Icon(FluentIcons.open_folder_horizontal,
+                        size: 15.0),
+                    onPressed: () async {
+                      FilePickerResult? result =
+                          await FilePicker.platform.pickFiles(
+                        type: FileType.custom,
+                        allowedExtensions: ['*'],
+                      );
+
+                      if (result != null) {
+                        widget.autoSuggestBox.text = result.files.single.path!;
+                      } else {
+                        // User canceled the picker
+                      }
+                    },
+                  ),
+                );
+              }),
+        ),
+        Container(
+          height: 10.0,
+        ),
+        const Text(
+          'Save location:',
+        ),
+        Container(
+          height: 5.0,
+        ),
+        Tooltip(
+          message: '(Optional) Path where to save the new instance',
+          child: TextBox(
+            controller: widget.locationController,
+            placeholder: 'Save location (optional)',
+            suffix: IconButton(
+              icon: const Icon(FluentIcons.open_folder_horizontal, size: 15.0),
+              onPressed: () async {
+                String? path = await FilePicker.platform.getDirectoryPath();
+                if (path != null) {
+                  widget.locationController.text = path;
+                } else {
+                  // User canceled the picker
+                }
+              },
+            ),
+          ),
+        ),
+        Container(
+          height: 10.0,
+        ),
+        turnkey
+            ? const Text(
+                'Warning: You selected a turnkey container. [Experimental]\n'
+                'As most of them use systemd and WSL currently does not '
+                'support systemd out of the box it will '
+                'be replaced with a fork of fake_systemd. This will start the '
+                'applications not on init but on console openings for more info '
+                'check the GitHub project\'s README.\n'
+                'To access the service you can use "ip a | grep inet" to find '
+                'the ip and then navigate to WSL-IP:PORT e.g. in your browser.',
+                style: TextStyle(fontStyle: FontStyle.italic))
+            : Container(),
+        !turnkey
+            ? const Text(
+                'Create default user (only on Debian/Ubuntu):',
+              )
+            : Container(),
+        !turnkey
+            ? Container(
+                height: 5.0,
+              )
+            : Container(),
+        !turnkey
+            ? Tooltip(
+                message: '(Optional) Username',
+                child: TextBox(
+                  controller: widget.userController,
+                  placeholder: '(Optional) User',
+                ),
+              )
+            : Container(),
+      ],
+    );
+  }
 }
