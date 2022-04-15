a05d8e710777a094a8bb898687c85d4e3b8675c5
Eric <me@bostrot.com>
2022-04-15 16:27:50 +0200

    Updated info dialog

--- lib/dialogs/info_dialog.dart
+++ lib/dialogs/info_dialog.dart
@@ -18,103 +18,83 @@ infoDialog(context, prefs, Function(String, {bool loading}) statusMsg,
           title: Center(child: Text('WSL Manager $currentVersion')),
           content: SizedBox(
             width: 400.0,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                TextButton(
-                    onPressed: () async {
-                      plausible.event(name: "url_clicked");
-                      await canLaunch('https://bostrot.com')
-                          ? await launch('https://bostrot.com')
-                          : throw 'Could not launch URL';
-                    },
-                    child: const Text(
-                      "Created by Bostrot",
-                    )),
-                TextButton(
-                    onPressed: () async {
-                      plausible.event(name: "git_clicked");
-                      launch('https://github.com/bostrot/wsl2-distro-manager');
-                    },
-                    child: const Text(
-                      "Visit GitHub",
-                    )),
-                TextButton(
-                    onPressed: () async {
-                      plausible.event(name: "changelog_clicked");
-                      launchURL(
-                          'https://github.com/bostrot/wsl2-distro-manager/'
-                                  'releases/tag/' +
-                              currentVersion);
-                    },
-                    child: const Text(
-                      "Changelog",
-                    )),
-                TextButton(
-                    onPressed: () async {
-                      plausible.event(name: "donate_clicked");
-                      launchURL('http://paypal.me/bostrot');
-                    },
-                    child: const Text(
-                      "Donate",
-                    )),
-                TextButton(
-                    onPressed: () {
-                      plausible.event(name: "libraries_clicked");
-                      dialog(
-                        context: context,
-                        item: "Dependencies",
-                        statusMsg: statusMsg,
-                        title: 'Dependencies',
-                        body: 'cupertino_icons: ^1.0.2'
-                            'desktop_window: ^0.4.0\n'
-                            'fluent_ui: ^3.9.1\n'
-                            'system_theme: ^1.0.1\n'
-                            'file_picker: ^4.5.1\n'
-                            'url_launcher: ^6.0.20\n'
-                            'dio: ^4.0.4\n'
-                            'package_info_plus: ^1.4.0\n'
-                            'bitsdojo_window: ^0.1.1+1\n'
-                            'plausible_analytics: ^0.1.2\n'
-                            'shared_preferences: ^2.0.13\n'
-                            'shelf_static: ^1.1.0\n',
-                        submitInput: false,
-                        centerText: true,
-                      );
-                    },
-                    child: const Text(
-                      "Dependencies",
-                    )),
-                TextButton(
-                    onPressed: () {
-                      plausible.event(name: "analytics_clicked");
-                      dialog(
+            height: 200.0,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const ClickableUrl(
+                    clickEvent: "url_clicked",
+                    url: 'https://bostrot.com',
+                    text: "Created by Bostrot",
+                  ),
+                  const ClickableUrl(
+                      clickEvent: "git_clicked",
+                      url: "https://github.com/bostrot/wsl2-distro-manager",
+                      text: "Visit GitHub"),
+                  const ClickableUrl(
+                      clickEvent: "changelog_clicked",
+                      url: "https://github.com/bostrot/wsl2-distro-manager/"
+                          "releases",
+                      text: "Changelog"),
+                  const ClickableUrl(
+                      clickEvent: "donate_clicked",
+                      url: "https://paypal.me/bostrot",
+                      text: "Donate"),
+                  ClickableText(
+                      clickEvent: "libraries_clicked",
+                      onPressed: () {
+                        dialog(
                           context: context,
-                          item: "Allow",
+                          item: "Dependencies",
                           statusMsg: statusMsg,
-                          title: 'Usage Data',
-                          body: 'Do you want to share anonymous usage data to '
-                              'improve this app?',
-                          submitText: 'Enable privacy mode',
+                          title: 'Dependencies',
+                          body: "",
+                          bodyIsWidget: true,
+                          bodyAsWidget: const DependencyList(),
                           submitInput: false,
-                          submitStyle: const ButtonStyle(),
-                          cancelText: 'Share usage data',
-                          onCancel: () {
-                            plausible.event(name: "privacy_off");
-                            prefs.setBool('privacyMode', false);
-                            plausible.enabled = true;
-                          },
-                          onSubmit: (inputText) {
-                            plausible.event(name: "privacy_on");
-                            prefs.setBool('privacyMode', true);
-                            plausible.enabled = false;
-                            statusMsg('Privacy mode enabled.');
-                          });
-                    },
-                    child: const Text(
-                      "Privacy",
-                    )),
-              ],
+                          centerText: true,
+                        );
+                      },
+                      text: "Dependencies"),
+                  ClickableText(
+                      clickEvent: "analytics_clicked",
+                      onPressed: () {
+                        bool privacyMode =
+                            prefs.getBool('privacyMode') ?? false;
+                        String privacyStatus = privacyMode
+                            ? "\"NOT sharing anonymous usage data\""
+                            : "\"Sharing anonymous usage data\"";
+                        dialog(
+                            context: context,
+                            item: "Allow",
+                            statusMsg: statusMsg,
+                            title: 'Usage Data',
+                            body:
+                                'Do you want to share anonymous usage data to '
+                                'improve this app? This is done via plausible '
+                                'a privacy-friendly and open source analytics '
+                                'tool.\n\n'
+                                'Current status: $privacyStatus',
+                            submitText: 'Do NOT share',
+                            submitInput: false,
+                            submitStyle: const ButtonStyle(),
+                            cancelText: 'Share',
+                            onCancel: () {
+                              plausible.event(name: "privacy_off");
+                              prefs.setBool('privacyMode', false);
+                              plausible.enabled = true;
+                            },
+                            onSubmit: (inputText) {
+                              plausible.event(name: "privacy_on");
+                              prefs.setBool('privacyMode', true);
+                              plausible.enabled = false;
+                              statusMsg('Privacy mode enabled.');
+                            });
+                      },
+                      text: "Privacy"),
+                ],
+              ),
             ),
           ),
           actions: [
@@ -126,4 +106,109 @@ infoDialog(context, prefs, Function(String, {bool loading}) statusMsg,
           ],
         );
       });
+}
+
+class DependencyList extends StatelessWidget {
+  const DependencyList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        ClickableDependency(name: "desktop_window"),
+        ClickableDependency(name: "fluent_ui"),
+        ClickableDependency(name: "system_theme"),
+        ClickableDependency(name: "file_picker"),
+        ClickableDependency(name: "url_launcher"),
+        ClickableDependency(name: "dio"),
+        ClickableDependency(name: "package_info_plus"),
+        ClickableDependency(name: "bitsdojo_window"),
+        ClickableDependency(name: "plausible_analytics"),
+        ClickableDependency(name: "shared_preferences"),
+        ClickableDependency(name: "shelf_static"),
+      ],
+    );
+  }
+}
+
+class ClickableDependency extends StatelessWidget {
+  const ClickableDependency({Key? key, required this.name}) : super(key: key);
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+              onPressed: () async {
+                await canLaunch("https://pub.dev/packages/$name")
+                    ? await launch("https://pub.dev/packages/$name")
+                    : null;
+              },
+              child: Text(name)),
+          TextButton(
+              onPressed: () async {
+                await canLaunch("https://pub.dev/packages/$name/license")
+                    ? await launch("https://pub.dev/packages/$name/license")
+                    : null;
+              },
+              child: const Text("(LICENSE)")),
+        ],
+      ),
+    );
+  }
+}
+
+class ClickableUrl extends StatelessWidget {
+  const ClickableUrl(
+      {Key? key,
+      required this.clickEvent,
+      required this.url,
+      required this.text})
+      : super(key: key);
+
+  final String clickEvent;
+  final String url;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: TextButton(
+          onPressed: () async {
+            plausible.event(name: clickEvent);
+            await canLaunch(url) ? await launch(url) : null;
+          },
+          child: Text(text)),
+    );
+  }
+}
+
+class ClickableText extends StatelessWidget {
+  const ClickableText(
+      {Key? key,
+      required this.clickEvent,
+      required this.onPressed,
+      required this.text})
+      : super(key: key);
+
+  final String clickEvent;
+  final Function() onPressed;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: TextButton(onPressed: onPressed, child: Text(text)),
+    );
+  }
 }
