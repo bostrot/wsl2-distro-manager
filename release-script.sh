b878b62a81105cd91136e8e17fca458ec96b16dc
Eric <me@bostrot.com>
2022-05-18 13:13:19 +0200

    Updated .gitlab-ci.yaml

--- release-script.sh
+++ release-script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # get version from pubspec
-version=$(cat pubspec.yaml | grep -o -P '(?<=version: ).*(?= #)')
+VERSION_NUM=$(cat pubspec.yaml | grep -o -P '(?<=version: ).*(?= #)')
 
 # copy files
 cp ./windows-dlls/* ./build/windows/runner/Release
 cd ./build/windows/runner/Release
-zip -r wsl2-distro-manager-v$version.zip .
+mv wsl2distromanager.msix ../wsl2distromanager-v$VERSION_NUM-signed-installer.msix
+zip -r wsl2-distro-manager-v$VERSION_NUM.zip .
 
 # check tag already exists
-if [ "$(echo $(gh release view --json tagName) | sed -e 's/{"tagName":"\(.*\)"}/\1/')" != "v$version" ]; 
+if [ "$(echo $(gh release view --json tagName) | sed -e 's/{"tagName":"\(.*\)"}/\1/')" != "v$VERSION_NUM" ]; 
 then 
-gh release create v$version wsl2-distro-manager-v$version.zip --notes "This is an automated release." 
+gh release create v$VERSION_NUM wsl2-distro-manager-v$version.zip wsl2distromanager_signed_installer.msix --notes "This is an automated release." 
 fi
