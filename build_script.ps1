5884a78077181251c6916f0889db3635773e72c0
Eric <me@bostrot.com>
2022-05-21 02:09:50 +0200

    Updated build script

--- build_script.ps1
+++ build_script.ps1
@@ -1,7 +1,13 @@
 flutter build windows
+
+# get version
+$pubspec_file = Get-Content '.\pubspec.yaml'
+$version = [regex]::Matches($pubspec_file, 'version: ([^/)]+) # Current version')[0].Groups[1].Value
+
 If((Get-Content .\pubspec.yaml | ForEach-Object{$_ -match "msix_config"}) -contains $true) {
     # already present
 } else {
-    (Get-Content .\certs\pubspec.yaml | Out-String) | Add-Content .\pubspec.yaml
+    # msix config with current version
+    (Get-Content .\certs\pubspec.yaml | Out-String).Replace("xxx", $version) | Add-Content .\pubspec.yaml
 }
 flutter pub run msix:create
