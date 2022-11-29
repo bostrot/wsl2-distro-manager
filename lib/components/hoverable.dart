dee8d16256c2f317f82fecd1f3298de82d3bc06a
Eric <eric@bostrot.com>
2022-11-29 20:11:42 +0100

    Added hoverable widget

---
+++ lib/components/hoverable.dart
@@ -0,0 +1,40 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/gestures.dart';
+
+class Hoverable extends StatefulWidget {
+  const Hoverable({Key? key, required this.child}) : super(key: key);
+  final Widget child;
+
+  @override
+  State<Hoverable> createState() => _HoverableState();
+}
+
+class _HoverableState extends State<Hoverable> {
+  bool isHovering = false;
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+        cursor: SystemMouseCursors.click,
+        onExit: (PointerExitEvent event) {
+          // Do not change state if already hovering
+          if (isHovering) {
+            setState(() {
+              isHovering = false;
+            });
+          }
+        },
+        onEnter: (PointerEnterEvent event) {
+          // Do not change state if already hovering
+          if (!isHovering) {
+            setState(() {
+              isHovering = true;
+            });
+          }
+        },
+        child: Opacity(
+          opacity: isHovering ? 0.5 : 1,
+          child: widget.child,
+        ));
+    ;
+  }
+}
