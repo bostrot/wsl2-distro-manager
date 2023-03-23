0848c7ebde40e1d0c9e50d6a947ba86cf8d08245
Eric <eric@bostrot.com>
2023-03-23 13:13:52 +0100

    Fixed tag

--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -95,10 +95,12 @@ class _NavbarState extends State<Navbar> {
             }
 
             // Custom matching for chinese (simplified and traditional)
-            if (language.contains("hans") || locale.languageCode == "zh") {
+            if (language.contains("hans")) {
               return const Locale('zh', 'CN');
             } else if (language.contains("hant")) {
               return const Locale('zh', 'TW');
+            } else if (locale.languageCode == "zh") {
+              return const Locale('zh', 'CN');
             }
 
             // No exact match, try language only
