b27246371ae8db87288dd1383fd531b389afdc97
Eric <eric@bostrot.com>
2023-04-17 12:01:23 +0200

    Added pathing helpers

--- lib/components/helpers.dart
+++ lib/components/helpers.dart
@@ -1,13 +1,15 @@
+import 'dart:io';
+
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wsl2distromanager/api/safe_paths.dart';
+import 'package:wsl2distromanager/components/constants.dart';
 import 'package:wsl2distromanager/nav/root_screen.dart';
 
 late String language;
 late SharedPreferences prefs;
 
-/// Get distro label from item
-/// @param item: distro name
-/// @returns String
+/// Get distro label from [item].
 String distroLabel(String item) {
   String? distroName = prefs.getString('DistroName_$item');
   if (distroName == null || distroName == '') {
@@ -21,7 +23,51 @@ Future initPrefs() async {
   prefs = await SharedPreferences.getInstance();
 }
 
+/// Global variables for global context access.
 class GlobalVariable {
   static final GlobalKey<RootPageState> root = GlobalKey<RootPageState>();
   static GlobalKey<NavigatorState> infobox = GlobalKey<NavigatorState>();
+}
+
+/// Return the general distro path. Distros are saved here by default.
+/// It will be created if it does not exist.
+///
+/// e.g. C:\WSL2-Distros\distros
+SafePath getDistroPath() {
+  String path = prefs.getString('DistroPath') ?? defaultPath;
+  return SafePath(path)..cd('distros');
+}
+
+/// Get the tmp folder path. This is used for the download of docker layers.
+/// It will be created if it does not exist.
+///
+/// e.g. C:\WSL2-Distros\tmp
+SafePath getTmpPath() {
+  return getDistroPath()
+    ..cdUp()
+    ..cd('tmp');
+}
+
+/// Get the instance path for the [name] instance.
+/// It will be created if it does not exist.
+///
+/// e.g. C:\WSL2-Distros\ubuntu
+SafePath getInstancePath(String name) {
+  String? instanceLocation = prefs.getString('Path_$name');
+  if (instanceLocation != null && instanceLocation.isNotEmpty) {
+    // Fix path for older versions
+    var safePath = SafePath(instanceLocation);
+    prefs.setString('Path_$name', safePath.path);
+    return safePath;
+  }
+  return getDistroPath()
+    ..cdUp()
+    ..cdUp()
+    ..cd(name);
+}
+
+/// Get the wslconfig path
+String getWslConfigPath() {
+  return SafePath('C:\\Users\\${Platform.environment['USERNAME']}')
+      .file('.wslconfig');
 }
