9cec56c8c70d257ee17f72d5fada95e5954c47ac
Eric <eric@bostrot.com>
2023-04-26 11:19:00 +0200

    Added WSL API docs

--- lib/api/wsl.dart
+++ lib/api/wsl.dart
@@ -13,6 +13,7 @@ import 'package:wsl2distromanager/components/constants.dart';
 import 'package:wsl2distromanager/components/helpers.dart';
 import 'package:wsl2distromanager/components/logging.dart';
 
+/// Used to store the instances of WSL in a list.
 class Instances {
   List<String> running = [];
   List<String> all = [];
@@ -21,7 +22,9 @@ class Instances {
 
 bool inited = false;
 
-/// WSL API
+/// This class is used to interact with WSL. It contains all the functions
+/// needed to interact with WSL based on Process.run and Process.start.
+/// Most functions will return the UTF8 converted stdout of the process.
 class WSLApi {
   WSLApi() {
     if (!inited) {
@@ -30,7 +33,9 @@ class WSLApi {
     }
   }
 
-  /// Get distro size
+  /// Get distro size of [distroName] a string with a GB suffix.
+  /// Returns null if size is 0.
+  /// e.g. "2.00 GB"
   String? getSize(String distroName) {
     String ext4Path = getInstancePath(distroName).file('ext4.vhdx');
     // Get size of distro
