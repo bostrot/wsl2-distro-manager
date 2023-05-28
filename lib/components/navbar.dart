926ce710555ef0791d1b87db50ff5c48ecf50829
Eric <eric@bostrot.com>
2023-05-28 09:14:25 +0200

    Upgraded flutter stable to v3.10.2

--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -74,14 +74,14 @@ class _NavbarState extends State<Navbar> {
             accentColor: appTheme.color,
             visualDensity: VisualDensity.standard,
             focusTheme: FocusThemeData(
-              glowFactor: is10footScreen() ? 2.0 : 0.0,
+              glowFactor: is10footScreen(context) ? 2.0 : 0.0,
             ),
           ),
           theme: FluentThemeData(
             accentColor: appTheme.color,
             visualDensity: VisualDensity.standard,
             focusTheme: FocusThemeData(
-              glowFactor: is10footScreen() ? 2.0 : 0.0,
+              glowFactor: is10footScreen(context) ? 2.0 : 0.0,
             ),
           ),
           locale: appTheme.locale,
